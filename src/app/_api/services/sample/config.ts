40983748a57dcc05cf93f0e987831b56a3af05ad
younyikim <rlakuku1221@gmail.com>
2025-04-11 16:26:14 +0900

    feat: Delete unused file n deps

--- src/app/_api/services/sample/config.ts
+++
@@ -1,8 +0,0 @@
-export const sampleEndpoints = {
-  getSample: () => 'https://jsonplaceholder.typicode.com/posts/1',
-};
-
-export const sampleKeys = {
-  all: ['sample'],
-  getSample: () => [...sampleKeys.all, 'get_sample'],
-};
